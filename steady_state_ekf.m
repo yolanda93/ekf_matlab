fc6a93d8a619d4ad5f605700f7c18843ecd8e0ca
Yolanda de la Hoz Simón <yolanda93h@gmail.com>
2016-05-01 19:38:32 +0100

    first commit

---
+++ steady_state_ekf.m
@@ -0,0 +1,12 @@
+% Consider a discrete plant with additive Gaussian noise 
+
+% Matrices represent the dynamics of this plant.
+A = [1.1269   -0.4940    0.1129;
+     1.0000         0         0;
+          0    1.0000         0];
+
+B = [-0.3832;
+      0.5919;
+      0.5191];
+
+C = [1 0 0];
